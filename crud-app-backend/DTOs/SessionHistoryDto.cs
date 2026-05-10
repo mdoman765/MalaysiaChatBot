be2d33eee3513890c9aa946d907da4ee3d875c29
Oman <mdoman765@gmail.com>
2026-05-10 18:37:44 +0600

    Add project files.

---
+++ crud-app-backend/DTOs/SessionHistoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace crud_app_backend.DTOs
+{
+    public class SessionResponse
+    {
+        public string Phone { get; set; } = default!;
+        public string CurrentStep { get; set; } = "INIT";
+        public string PreviousStep { get; set; } = "INIT";
+        public string TempData { get; set; } = "{}";
+        public DateTime UpdatedAt { get; set; }
+        public bool IsNew { get; set; }
+    }
+
+}
